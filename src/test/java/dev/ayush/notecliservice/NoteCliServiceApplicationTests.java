bd7d68a436b9e26613335d847d2b93108c9dc35f
ayush-devhub <ayush.devhub@gmail.com>
2025-09-13 19:14:49 +0530

    fix(git): add remaining unstaged files from root directory

---
+++ src/test/java/dev/ayush/notecliservice/NoteCliServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ayush.notecliservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteCliServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
