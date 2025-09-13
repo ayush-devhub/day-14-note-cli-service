6f7e7ba06a6ef1451852f183d9337729ef11e2e1
ayush-devhub <ayush.devhub@gmail.com>
2025-09-13 21:55:05 +0530

    feat(model): add Note model

--- src/main/java/dev/ayush/notecliservice/model/Note.java
+++ src/main/java/dev/ayush/notecliservice/model/Note.java
@@ -1,4 +1,40 @@
 package dev.ayush.notecliservice.model;
 
+import java.time.LocalDateTime;
+
 public class Note {
+
+    private final int id;
+    private final LocalDateTime createdAt;
+    private String title;
+    private String content;
+    private LocalDateTime updatedAt;
+
+    public Note(int id, LocalDateTime createdAt, String title, String content, LocalDateTime updatedAt) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.title = title;
+        this.content = content;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
 }
